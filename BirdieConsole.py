32b7d41bb2a7e28eea4a2ccc6a7fbb301944b67a
Devendra <54927248+tambatd@users.noreply.github.com>
2022-12-31 14:25:17 -0500

    Create BirdieConsole.py

---
+++ BirdieConsole.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import tweepy
+import os
+
+ 
+api_key = ""
+api_secrets = ""
+access_token = ""
+access_secret = ""
+ 
+auth = tweepy.OAuthHandler(api_key, api_secrets)
+auth.set_access_token(access_token, access_secret)
+api = tweepy.API(auth)
+  
+def tweeter():
+  tweet = input("🐦: ")
+  if tweet == "exit":
+    exit()
+  api.update_status(tweet)
+  print("✨: " + tweet)
+  tweeter()
+
+def main():
+    os.system('clear')
+    tweeter()
+
+if __name__ == "__main__":
+    main()
